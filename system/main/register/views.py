a0a080b206b1e6a3fe102027db3294739b527511
Margaid <ola.kilijanska@gmail.com>
2022-12-12 00:02:00 +0100

    Stworzenie strony głównej, logowania i oczekiwania na potwierdzenie rejestracji.

--- system/main/register/views.py
+++ system/main/register/views.py
@@ -26,20 +26,22 @@ def register(request):
 
 '''
 
+def home(response):
+    return render(response, 'home/home.html')
+
+#rejestracja użytkownika
 def register(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         p_reg_form = ProfileForm(request.POST)
         if form.is_valid() and p_reg_form.is_valid():
             user = form.save()
-            user.refresh_from_db()  # load the profile instance created by the signal
+            user.refresh_from_db()  
             p_reg_form = ProfileForm(request.POST, instance=user.profile)
             p_reg_form.full_clean()
-            #user.customuser.firma = p_reg_form.cleaned_data.get('firma')
-            #user.customuser.operator = p_reg_form.cleaned_data.get('operator')
             p_reg_form.save()
             #messages.success(request, f'Your account has been sent for approval!')
-            return redirect('register.html')
+            return redirect('waiting/')
     else:
         form = SignUpForm()
         p_reg_form = ProfileForm()
@@ -49,8 +51,8 @@ def register(request):
         'p_reg_form': p_reg_form
     })
 
+#def login(response):
+ #   return render(response,'registration/login.html')
 
-
-#def update_profile(request, user_id):
-#    user = User.objects.get(pk=user_id)
-#    user.save()
+def waiting(response):
+    return render(response, 'waiting/waiting.html')
